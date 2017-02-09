e9250e0ca36d51d03d74a0870d32ba4f2860826c
Nicolas Ferrary <ferraryn@gmail.com>
2017-02-09 17:38:34 +0100

    doses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show]
+  resources :cocktails, only: [:index, :show] do
+    resources :doses, only: [ :new, :create ]
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@ require 'open-uri'
 
 
 Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+ingr_array = []
+cocktail_array = []
 
 file = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
@@ -10,11 +14,10 @@ ingredient_serialized = open(file).read
 ingredient = JSON.parse(ingredient_serialized)
 
 ingredient["drinks"].each do |ingredient|
-  Ingredient.create!(name: ingredient["strIngredient1"])
+  ingr = Ingredient.create!(name: ingredient["strIngredient1"])
+  ingr_array << ingr
 end
 
-
-Cocktail.destroy_all
 
 url = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
 
@@ -22,5 +25,15 @@ cocktail_serialized = open(url).read
 cocktail = JSON.parse(cocktail_serialized)
 
 cocktail["drinks"].each do |cocktail|
-  Cocktail.create!(name: cocktail["strDrink"])
+  cocktail = Cocktail.create!(name: cocktail["strDrink"])
+  cocktail_array << cocktail
 end
+
+cocktail_array.each do |cocktail|
+  n = rand(1..5)
+  ing = ingr_array.sample(n)
+  ing.each do |ingredient|
+    Dose.create!(description: "Gently add #{rand(1..10)} cl of #{ingredient.name}", cocktail: cocktail, ingredient: ingredient)
+  end
+end
+
