b283b1b2a39c9b9b73bb897d8aed785b9750006c
Nicolas Ferrary <ferraryn@gmail.com>
2017-02-09 12:17:46 +0100

    models

---
+++ app/models/ingredient.rb
@@ -0,0 +1,16 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+  validates :name, presence: true, uniqueness: true
+  before_destroy :check_for_doses
+
+
+  private
+
+  def check_for_doses
+    if doses.count > 0
+      errors.add_to_base("cannot delete ingredient when used by a cocktail")
+      return false
+    end
+  end
+
+end
