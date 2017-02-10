5beac2e1e8b7ef937facc1af8cc6b094252ad6bd
Nicolas Ferrary <ferraryn@gmail.com>
2017-02-10 12:31:55 +0100

    put cache again

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,7 +29,7 @@ class CocktailsController < ApplicationController
     end
 
     def cocktail_params
-      params.require(:cocktail).permit(:name, :photo)
+      params.require(:cocktail).permit(:name, :photo, :photo_cache)
     end
 
 end
